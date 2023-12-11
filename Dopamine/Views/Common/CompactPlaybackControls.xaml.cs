183ad0348bfa83b8ccc59077d6f55ee18acefabd
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:23:59 +0530

    update file

---
+++ Dopamine/Views/Common/CompactPlaybackControls.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Dopamine.Views.Common
+{
+    public partial class CompactPlaybackControls : UserControl
+    {
+        public CompactPlaybackControls()
+        {
+            InitializeComponent();
+        }
+    }
+}

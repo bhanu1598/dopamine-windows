09b59bb719d8daa20e407a7a112db561215d8786
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:25:01 +0530

    update file

---
+++ Dopamine/Views/FullPlayer/Collection/CollectionMenu.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Dopamine.Views.FullPlayer.Collection
+{
+    public partial class CollectionMenu : UserControl
+    {
+        public CollectionMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}

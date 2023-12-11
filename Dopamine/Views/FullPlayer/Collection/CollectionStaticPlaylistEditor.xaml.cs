d252b018ac1cb864af3ae2c26aef0dad95ab20e8
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:25:04 +0530

    update file

---
+++ Dopamine/Views/FullPlayer/Collection/CollectionStaticPlaylistEditor.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Dopamine.Views.FullPlayer.Collection
+{
+    public partial class CollectionStaticPlaylistEditor : UserControl
+    {
+        public CollectionStaticPlaylistEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}

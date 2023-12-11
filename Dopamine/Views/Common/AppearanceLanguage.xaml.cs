5eab578ca480eb0e978998b2f228cebcbca0fe97
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:23:54 +0530

    update file

---
+++ Dopamine/Views/Common/AppearanceLanguage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Dopamine.Views.Common
+{
+    public partial class AppearanceLanguage : UserControl
+    {
+        public AppearanceLanguage()
+        {
+            InitializeComponent();
+        }
+    }
+}

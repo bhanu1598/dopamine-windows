d021a961c5f370ff0003dda34961c57d7013bf87
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:24:35 +0530

    update file

---
+++ Dopamine.Core/Api/Lastfm/LastFmBiography.cs
@@ -0,0 +1,9 @@
+namespace Dopamine.Core.Api.Lastfm
+{
+    public class LastFmBiography
+    {
+        public string Published { get; set; }
+        public string Summary { get; set; }
+        public string Content { get; set; }
+    }
+}

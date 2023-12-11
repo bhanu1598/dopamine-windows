5480a884f488bb44d823ea8ce35921e8bccb6d41
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:22:00 +0530

    update file

---
+++ Dopamine.Core/Base/UpdateInformation.cs
@@ -0,0 +1,8 @@
+namespace Dopamine.Core.Base
+{
+    public static class UpdateInformation
+    {
+        public static string AutomaticDownloadLink = "http://www.digimezzo.com/content/software/dopamine/.update";
+        public static string DownloadLink = "http://www.digimezzo.com/content/software/dopamine";
+    }
+}

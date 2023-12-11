2fcc33fede14092e2219961d119f0a42f2572234
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:24:22 +0530

    update file

---
+++ Dopamine/Views/Common/TimeControl.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Dopamine.Views.Common
+{
+    public partial class TimeControl : UserControl
+    {
+        public TimeControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

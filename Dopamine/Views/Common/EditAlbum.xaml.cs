df7f5f1132878190151f498ae366e45b39657f4b
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:24:02 +0530

    update file

---
+++ Dopamine/Views/Common/EditAlbum.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Dopamine.Views.Common
+{
+    public partial class EditAlbum : UserControl
+    {
+        public EditAlbum()
+        {
+            InitializeComponent();
+        }
+    }
+}

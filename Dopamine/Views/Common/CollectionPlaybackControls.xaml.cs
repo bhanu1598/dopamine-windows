5c55b775cdd6968ce605e17a8aa0b72457a416cc
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:23:58 +0530

    update file

---
+++ Dopamine/Views/Common/CollectionPlaybackControls.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Dopamine.Views.Common
+{
+    public partial class CollectionPlaybackControls : UserControl
+    {
+        public CollectionPlaybackControls()
+        {
+            InitializeComponent();
+        }
+    }
+}

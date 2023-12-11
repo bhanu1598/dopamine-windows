8080c1789e237248bc0d066fb4b1b36f67e54bb2
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:22:02 +0530

    update file

---
+++ Dopamine.Core/Enums/SpectrumEnums.cs
@@ -0,0 +1,16 @@
+namespace Dopamine.Core.Enums
+{
+    public enum SpectrumChannel
+    {
+        Left = 1,
+        Right = 2,
+        Stereo = 3,
+    }
+
+    public enum SpectrumStyle
+    {
+        Flames = 1,
+        Lines = 2,
+        Bars = 3
+    }
+}

4f8580fd6bf709584c86915236312a31d71d4287
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:24:11 +0530

    update file

---
+++ Dopamine/Views/Common/NanoPlayerControls.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Dopamine.Views.Common
+{
+    public partial class NanoPlayerControls : UserControl
+    {
+        public NanoPlayerControls()
+        {
+            InitializeComponent();
+        }
+    }
+}

1a2aecacfa65468c1e87881917f94b0c0d3ca5da
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:24:13 +0530

    update file

---
+++ Dopamine/Views/Common/NowPlayingPlaybackControls.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Dopamine.Views.Common
+{
+    public partial class NowPlayingPlaybackControls : UserControl
+    {
+        public NowPlayingPlaybackControls()
+        {
+            InitializeComponent();
+        }
+    }
+}

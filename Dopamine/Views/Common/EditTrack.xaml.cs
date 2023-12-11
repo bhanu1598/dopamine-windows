2f41866c024e0d3d5ef1b098af8e8c643b37d080
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:24:03 +0530

    update file

---
+++ Dopamine/Views/Common/EditTrack.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Dopamine.Views.Common
+{
+    public partial class EditTrack : UserControl
+    {
+        public EditTrack()
+        {
+            InitializeComponent();
+        }
+    }
+}
